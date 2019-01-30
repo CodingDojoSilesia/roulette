76793314a035e8e71ad8dc3ce42c78be9d0fa0c1
Arkadiusz Nowakowski <arkadiusz.nowakowski@us.edu.pl>
2019-01-30 22:05:36 +0100

    fix: removing what Firemark said I need to remove xD

--- obfuscated/obfuscate.php
+++
@@ -1,8 +0,0 @@
-<?php
-require __DIR__ . '/Obfuscator.php';
-
-$filename = __DIR__ . '/api/index_orginal'; // A PHP filename (without .php) that you want to obfuscate
-$sData = file_get_contents($filename . '.php');
-$sData = str_replace(array('<?php', '<?', '?>'), '', $sData); // Strip PHP open/close tags
-$sObfusationData = new Obfuscator($sData, 'Class/Code NAME');
-file_put_contents($filename . '_obfuscated.php', '<?php ' . "\r\n" . $sObfusationData);
